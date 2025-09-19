09ff8f95e518c997eaf3084cef90d64072e87003
MRRAHMAN <mrrahman@MRRAHMANs-MacBook-Pro.local>
2025-09-19 22:49:48 +0600

    :up: Dynamic Pages

--- src/app/features/home/trending-courses/trending-courses.component.ts
+++ src/app/features/home/trending-courses/trending-courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-trending-courses',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './trending-courses.component.html',
   styleUrl: './trending-courses.component.scss'
 })
